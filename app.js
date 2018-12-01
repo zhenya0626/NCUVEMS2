b390fc6590a60db102dc57f81447ef7bc110f755
zhenya0626 <riana0626@yaoo.co.jp>
2018-12-01 15:50:39 +0900

    add favicon and methodOverride

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var favicon = require('serve-favicon');
+var methodOverride = require('method-override');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,16 +15,19 @@ var perminRouter = require('./routes/permin');
 var roomsRouter = require('./routes/rooms');
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico'))); 
+app.use(favicon(path.join(__dirname, 'public', '/images/favicon.ico')));
+app.use(methodOverride());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
