d3d0099e713ffab316854e06349b6b37957372ef
内間理亜奈 <riana0626@yahoo.co.jp>
2018-12-01 15:16:53 +0900

    favicon

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico'))); 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
